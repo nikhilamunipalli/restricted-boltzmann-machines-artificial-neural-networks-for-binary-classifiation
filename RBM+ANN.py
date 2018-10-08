53beb8f8feea32abe564eeb4d5c3e69fcc73bacf
Nikhila Munipalli <nikhilamunipalli2008@gmail.com>
2018-10-09 00:43:05 +0530

    Update RBM+ANN.py

--- RBM+ANN.py
+++ RBM+ANN.py
@@ -96,7 +96,7 @@ class RBM():
         p_v_given_h = torch.sigmoid(activation)
         return p_v_given_h, torch.bernoulli(p_v_given_h)
     def train(self,v0,vk,ph0,phk):
-        self.W += torch.mm(v0.t(),ph0) - torch.mm(vk.t(),phk)
+        self.W += (torch.mm(v0.t(),ph0) - torch.mm(vk.t(),phk)).t()
         self.a += torch.sum((ph0-phk),0)
         self.b += torch.sum((v0-vk),0)
 
